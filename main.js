c4839fc741df52b412e5afd199bbd272d5d9d780
Wendel Bueno <wendelbueno99@gmail.com>
2023-10-28 13:02:46 -0300

    colocando <script></script>

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 function validarFormulario() {
     var numberA = parseFloat(document.getElementById("numberA").value);
-    var numberB = parseFloat(document.getElementById("numberb").value);
+    var numberB = parseFloat(document.getElementById("numberB").value);
 
     if (numberB > numberA) {
         document.getElementById("resultado").textContent = "Formulário válido: B é maior que A.";
     } else {
-        document.getElementById("resultado").textContent = "Formulário inválido: A é maior que A";
+        document.getElementById("resultado").textContent = "Formulário inválido: B não é maior que A.";
     }
 }
-
-
-
